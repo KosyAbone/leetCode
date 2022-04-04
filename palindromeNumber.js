ea87bfad92a39ee2e850aba1be1f1aa7b17827f1
KOSSY <abonekosiso@gmail.com>
2022-04-04 23:55:58 +0100

    change

--- palindromeNumber.js
+++ palindromeNumber.js
@@ -23,13 +23,12 @@ Input: x = 10
 Output: false
 Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 
-
 */
 
-/**
- * @param {number} x
- * @return {boolean}
- */
+// /**
+//  * @param {number} x
+//  * @return {boolean}
+//  */
  var isPalindrome = function(x) {
     return x === Number(String(x).split('').reverse().join('')) ? true : false
  };
