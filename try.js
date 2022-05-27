0f3415f5da50c553ed141a9a7ce6f0cc6e14a4dc
KOSSY <abonekosiso@gmail.com>
2022-05-27 23:59:26 +0100

    new file

--- try.js
+++ try.js
@@ -1,1 +1,17 @@
-console.log(Boolean('h'))
+const arr = [7, 1, 3, 4, 1, 7]
+
+
+function minimumDistances(a) {
+    const copy = [...a]
+    const resultArr = [];
+    for(let i = 0; i < a.length; i++){
+        copy[i] = -1;
+        if(copy.indexOf(a[i]) > 1){
+            resultArr.push(copy.indexOf(a[i]) - i)
+            // console.log(found)
+        }
+        console.log(resultArr)
+    }
+}
+
+console.log(minimumDistances(arr))
