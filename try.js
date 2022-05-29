519f31d22dad836bae375181f8d7a24f015e56bb
KOSSY <abonekosiso@gmail.com>
2022-05-29 23:34:37 +0100

    added

--- try.js
+++ try.js
@@ -1,17 +1,7 @@
-const arr = [7, 1, 3, 4, 1, 7]
+const obj = {}
 
-
-function minimumDistances(a) {
-    const copy = [...a]
-    const resultArr = [];
-    for(let i = 0; i < a.length; i++){
-        copy[i] = -1;
-        if(copy.indexOf(a[i]) > 1){
-            resultArr.push(copy.indexOf(a[i]) - i)
-            // console.log(found)
-        }
-        console.log(resultArr)
-    }
+const arr = ['aa', 'bag', 'rul', 'bag', 'rul', 'egg']
+for(let i of arr){
+    obj[i] = 1 + (obj[i] || 0)
 }
-
-console.log(minimumDistances(arr))
+console.log(obj)
