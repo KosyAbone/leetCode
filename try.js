1861a70564ec6f06b46d523d316b9bb8b06bef80
KOSSY <abonekosiso@gmail.com>
2022-06-12 23:44:32 +0100

    more

--- try.js
+++ try.js
@@ -1,7 +1,7 @@
-const obj = {}
+// const obj = {}
 
-const arr = ['aa', 'bag', 'rul', 'bag', 'rul', 'egg']
-for(let i of arr){
-    obj[i] = 1 + (obj[i] || 0)
-}
-console.log(obj)
+// const arr = ['aa', 'bag', 'rul', 'bag', 'rul', 'egg']
+// for(let i of arr){
+//     obj[i] = 1 + (obj[i] || 0)
+// }
+// console.log(obj)
