ffd6c14116c299ce10f8debd98687c0a0b5093ca
KOSSY <abonekosiso@gmail.com>
2022-06-27 23:57:08 +0100

    removed  program

--- try.js
+++ try.js
@@ -3,9 +3,9 @@ let str = 'Golden Stack'
 function reverse(word){
     let result = '';
     for(let i = word.length - 1; i >= 0; i--){
-//         result += word[i];
-//     }
-//     return result
-// }
+        result += word[i];
+    }
+    return result
+}
 
-// console.log(reverse(str))
+console.log(reverse(str))
