07e85d579be568e68d48980f576cf614cbc1a364
KOSSY <abonekosiso@gmail.com>
2023-04-21 23:31:23 -0400

    new add

--- try.js
+++ try.js
@@ -1,9 +1,9 @@
 let str = 'Golden Stack'
 
-function reverse(word){
-    let result = '';
-    for(let i = word.length - 1; i >= 0; i--){
-        result += word[i];
-    }
-    return result
-}
+// function reverse(word){
+//     let result = '';
+//     for(let i = word.length - 1; i >= 0; i--){
+//         result += word[i];
+//     }
+//     return result
+// }
